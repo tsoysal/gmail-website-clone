03d70bd00e374e875c0fb4ce4baf9537a22fd321
Tahaberk SOYSAL <berksysl.g@gmail.com>
2023-06-16 00:12:32 +0300

    Section 2 completed

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const features = document.getElementsByClassName("feature-container")
-const featuresArr = Object.values(features)
-const imageContainer = document.getElementById("desktop-container")
 
-featuresArr.map((feature, index) => feature.addEventListener("click", () => {
-    var current = document.getElementsByClassName("active");
-    current[0].classList.remove("active")
-    feature.className += " active";
+const features1 = document.getElementsByClassName("sec1-container")
+const features2 = document.getElementsByClassName("sec2-container")
+
+
+const features1Arr = Object.values(features1)
+const features2Arr = Object.values(features2)
+
+
+const imageContainer = document.getElementById("desktop-container")
+const imageContainer2 = document.getElementById("desktop-container2")
+
+features1Arr.map((feature, index) => feature.addEventListener("click", () => {
+    var current = document.getElementsByClassName("active1");
+    current[0].classList.remove("active1")
+    feature.className += " active1";
     imageContainer.src = `./images/graphics${index+1}.png`;
+}))
+
+features2Arr.map((feature, index) => feature.addEventListener("click", () => {
+    var current = document.getElementsByClassName("active2");
+    current[0].classList.remove("active2")
+    feature.className += " active2";
+    imageContainer2.src = `./images/graphics${index+5}.png`;
 }))
 
